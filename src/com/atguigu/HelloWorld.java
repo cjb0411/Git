712fb279d6fa635ddb4c9fbe62e6f12d1cf572f0
cjb0411 <cjb0411@163.com>
2016-11-01 21:01:48 +0800

    update 22

--- src/com/atguigu/HelloWorld.java
+++ src/com/atguigu/HelloWorld.java
@@ -3,8 +3,10 @@ package com.atguigu;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		system.out.println("hello");
-
+		System.out.println("hello");
+		System.out.println("world");
+	
+		
 	}
 
 }
